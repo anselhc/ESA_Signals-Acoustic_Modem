d3f7f0f651a942332955bc55e462cccbcc1be699
anselhc <aharriscrowne@olin.edu>
2026-04-24 14:49:46 -0400

    flawed reciever code

--- modem_rx_starter.m
+++ modem_rx_starter.m
@@ -12,14 +12,19 @@ start_idx = find_start_of_signal(y_r,x_sync);
 y_t = y_r(start_idx+length(x_sync):end); % y_t is the signal which starts at the beginning of the transmission
 
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%  Put your decoder code here
-%%
-%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
+t_vals = [0:1/Fs:(39660/Fs)-1/Fs];
+cosine_transform = cos(2*pi*f_c*t_vals);
+x_d = y_t.*cosine_transform.*sinc(t_vals*2*f_c)*2000;
 
 % convert to a string assuming that x_d is a vector of 1s and 0s
 % representing the decoded bits.
+for i=1:length(x_d)
+    if x_d(i) > 0
+        x_d(i) = 1;
+    else
+        x_d(i) = 0;
+    end
+end
+x_d = x_d(1:39656);
 BitsToString(x_d)
 
